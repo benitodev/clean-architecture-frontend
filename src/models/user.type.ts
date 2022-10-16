c23cb53690721aed3f464739ee4ea0549e028d5a
benitodev <benideveloperr@gmail.com>
2022-10-16 19:14:24 -0300

    Add Entity User by Clean Architecture

---
+++ src/models/user.type.ts
@@ -0,0 +1,40 @@
+interface UserAssociatedUrl {
+    name: string;
+    url: string;
+}
+
+export interface ApiUser {
+    id: number,
+    name: string,
+    status: string,
+    species: string,
+    type: string,
+    gender: string,
+    origin: UserAssociatedUrl,
+    location: UserAssociatedUrl,
+    image: string,
+    episode: string[],
+    url: string,
+    created: string
+}
+
+
+export interface User {
+    id?: number,
+    name?: string,
+    status?: string,
+    species?: string,
+}
+
+
+const EmptyUserAssociatedUrl = {
+    name: '',
+    url: ''
+}
+
+export const initialUser: User = {
+    id: 0,
+    name: '',
+    status: '',
+    species:  '',
+}
